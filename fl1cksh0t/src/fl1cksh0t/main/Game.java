da483d615ae24a8d95dd413061f9389a93976af2
Evin Kierans <44497184+Real-EviN@users.noreply.github.com>
2019-01-13 02:56:22 +0000

    Blue v3ry b374

--- fl1cksh0t/src/fl1cksh0t/main/Game.java
+++ fl1cksh0t/src/fl1cksh0t/main/Game.java
@@ -10,7 +10,7 @@ public class Game extends Canvas implements Runnable
 	
 	public Game()
 	{
-		new Window(WIDTH, HEIGHT, "fl1cksh0t version: Very Beta", this);
+		new Window(WIDTH, HEIGHT, "fl1cksh0t version: V3ry B374", this);
 	}
 	
 	public synchronized void start()
