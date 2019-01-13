c08902011c95d39226b328baef3b9ea15e88cafb
Real-EviN <evin.kierans100@gmail.com>
2019-01-13 02:52:00 +0000

    Yellow

---
+++ fl1cksh0t/src/fl1cksh0t/main/Game.java
@@ -0,0 +1,31 @@
+package fl1cksh0t.main;
+
+import java.awt.Canvas;
+
+public class Game extends Canvas implements Runnable
+{
+	private static final long serialVersionUID = -3316329364487365225L;
+
+	public static final int WIDTH = 800, HEIGHT = 600;
+	
+	public Game()
+	{
+		new Window(WIDTH, HEIGHT, "Flickshot version: Very Beta", this);
+	}
+	
+	public synchronized void start()
+	{
+		
+	}
+	
+	public void run()
+	{
+		
+	}
+	
+	public static void main(String args[])	//main function
+	{
+		new Game();
+	}
+	
+}
