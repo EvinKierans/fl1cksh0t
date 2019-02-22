e7e23b9a85548d637cad78140faaefb940014529
Evin <evin.kierans100@gmail.com>
2019-02-22 18:43:49 +0000

    General optimisation updates

--- fl1cksh0t/src/fl1cksh0t/main/graphics/Render3d.java
+++ fl1cksh0t/src/fl1cksh0t/main/graphics/Render3d.java
@@ -76,8 +76,8 @@ public class Render3d extends Render {
 
 				zBuffer[x + y * width] = z;
 
-				//pixels[x + y * width] = ((xPix & 15) * 16) | ( (yPix & 15) * 16)  << 8;
-				pixels[x + y * width] = Texture.floor.pixels[(xPix & 7) + (yPix & 7) * 8];
+				pixels[x + y * width] = ((xPix & 15) * 16) | ( (yPix & 15) * 16)  << 8;
+				//pixels[x + y * width] = Texture.floor.pixels[(xPix & 7) + (yPix & 7) * 8];
 
 				//ultimate render distance limitation
 				if(z > renderDistance) {
